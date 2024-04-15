6261531c45c42d5e3e0568db1167fd9436573a40
MentheCamomille <s.s.1604manganise@gmail.com>
2024-04-15 14:23:18 +0200

    com

--- comprojet/authentication/serializers.py
+++ comprojet/authentication/serializers.py
@@ -2,13 +2,16 @@ from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework import serializers
 from django.contrib.auth.models import User
 
+# serializer for obtaining token pair with username included
 class ATokenObtainPairSerializer(TokenObtainPairSerializer):
     @classmethod
     def get_token(cls, user):
+        # get token from parent class
         token = super().get_token(user)
         token['username'] = user.username
         return token
-    
+
+# serializer for user model
 class UserSerializer(serializers.ModelSerializer):
     class Meta: 
         model = User
